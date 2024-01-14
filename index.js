1b99bd193b42c355a5c697ea735cea74f599bc83
Anh Do <anhdodr@gmail.com>
2024-01-14 12:15:19 +0700

    edit javascript

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("form submit");
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+function handleMouseOver(evt){
+	evt.preventDefault();
+	alert("you are so cool");
+}
+
+let pic = document.querySelector('#picture')
+pic.addEventListener("mouseover", handleMouseOver)
